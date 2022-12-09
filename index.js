c904716cbb1def606646345d5849e27b51d2fcc4
clairox <claireq620@gmail.com>
2022-12-09 13:19:32 -0500

    Added middleware to make secure = true

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ app.use(
 		saveUninitialized: false,
 		cookie: {
 			maxAge: 30 * 24 * 60 * 60 * 1000,
-			secure: false,
-			sameSite: 'none',
+			secure: true,
+			sameSite: 'lax',
 		},
 		store: new pgSession({
 			//TODO: when deleting session cookie in browser, cookie stays in store. idk what to do lol
