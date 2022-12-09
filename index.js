cce6e611a86aa6b88c03c5d7d2648e26739f89b7
clairox <claireq620@gmail.com>
2022-12-09 11:36:26 -0500

    Testing

--- index.js
+++ index.js
@@ -1,79 +1,79 @@
 const express = require("express");
-// const cors = require("cors");
-// const passport = require("passport");
-// const session = require("express-session");
-// const pgSession = require("connect-pg-simple")(session);
-// const logger = require("morgan");
-// const path = require("path");
+const cors = require("cors");
+const passport = require("passport");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const logger = require("morgan");
+const path = require("path");
 
-// const { pgPool } = require("./db");
+const { pgPool } = require("./db");
 
-// require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-// const corsOptions = {
-// 	origin: process.env.SITEURL,
-// 	credentials: true,
-// };
+const corsOptions = {
+	origin: process.env.SITEURL,
+	credentials: true,
+};
 
-// /* middleware */
-// app.use(cors(corsOptions));
+/* middleware */
+app.use(cors(corsOptions));
 
-// app.use((req, res, next) => {
-// 	res.setHeader(
-// 		"Access-Control-Allow-Origin",
-// 		process.env.SITEURL
-// 	);
-// 	res.setHeader(
-// 		"Access-Control-Allow-Methods",
-// 		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
-// 	);
-// 	res.setHeader(
-// 		"Access-Control-Allow-Headers",
-// 		"Origin,X-Requested-With,content-type"
-// 	);
-// 	res.setHeader("Access-Control-Allow-Credentials", true);
-// 	next();
-// });
+app.use((req, res, next) => {
+	res.setHeader(
+		"Access-Control-Allow-Origin",
+		process.env.SITEURL
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin,X-Requested-With,content-type"
+	);
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	next();
+});
 
-// app.use(logger("dev"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, "../client/public")));
-// app.use(
-// 	session({
-// 		secret: process.env.SESSION_SECRET_KEY,
-// 		resave: false,
-// 		saveUninitialized: false,
-// 		cookie: {
-// 			maxAge: 30 * 24 * 60 * 60 * 1000,
-// 		},
-// 		store: new pgSession({
-// 			//TODO: when deleting session cookie in browser, cookie stays in store. idk what to do lol
-// 			pool: pgPool,
-// 			createTableIfMissing: true,
-// 		}),
-// 	})
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../client/public")));
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET_KEY,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 30 * 24 * 60 * 60 * 1000,
+		},
+		store: new pgSession({
+			//TODO: when deleting session cookie in browser, cookie stays in store. idk what to do lol
+			pool: pgPool,
+			createTableIfMissing: true,
+		}),
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// /* routes */
-// const indexRouter = require("./routes/indexRoute");
-// const userRouter = require("./routes/userRoute");
-// const watchlistRouter = require("./routes/watchlistRoute");
-// const itemRouter = require("./routes/itemRoute");
+/* routes */
+const indexRouter = require("./routes/indexRoute");
+const userRouter = require("./routes/userRoute");
+const watchlistRouter = require("./routes/watchlistRoute");
+const itemRouter = require("./routes/itemRoute");
 
-// const version = process.env.API_VERSION;
-// app.use(`/${version}/`, indexRouter);
-// app.use(`/${version}/users`, userRouter);
-// app.use(`/${version}/watchlists`, watchlistRouter);
-// app.use(`/${version}/items`, itemRouter);
+const version = process.env.API_VERSION;
+app.use(`/${version}/`, indexRouter);
+app.use(`/${version}/users`, userRouter);
+app.use(`/${version}/watchlists`, watchlistRouter);
+app.use(`/${version}/items`, itemRouter);
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.send('Hello World!!')
   })
 
 app.listen(port, () => {
