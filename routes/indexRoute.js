7d9de567ac0aa40d27abefaf19006696a0c8316f
clairox <claireq620@gmail.com>
2022-12-12 12:41:02 -0500

    Change logout to method to GET

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -17,7 +17,7 @@ router.post("/login", passport.authenticate("local-login"), (req, res) => {
 	}
 });
 
-router.post("/logout", (req, res) => {
+router.get("/logout", (req, res) => {
 	req.logout(() => {
 		req.session.destroy(() => {
 			return res
