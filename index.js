ee03c24ab94b22371745649e4eab0b23bf4fdcb0
clairox <claireq620@gmail.com>
2022-12-12 12:49:52 -0500

    Add sameSite: none

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ app.use(
 		saveUninitialized: false,
 		cookie: {
 			maxAge: 30 * 24 * 60 * 60 * 1000,
-			secure: true
+			secure: true,
+			sameSite: 'none'
 		},
 		store: new pgSession({
 			//TODO: when deleting session cookie in browser, cookie stays in store. idk what to do lol
