38731c99d1ef4a857c816163e8385f3267f1b40e
Claire Q <claireq620@gmail.com>
2023-01-15 16:40:31 -0500

    Added new endpoints

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,6 @@
 const { pgPool } = require("../db");
+const bcrypt = require("bcrypt");
+
 
 const router = require("express").Router();
 
@@ -22,8 +24,7 @@ router.get("/sessionUser", (req, res) => {
 					id: user.id,
 					email: user.email,
 					firstName: user.firstname,
-					lastName: user.lastname,
-					time: new Date()
+					lastName: user.lastname
 				});
 			} else {
 				return res.status(404).json();
@@ -56,6 +57,91 @@ router.get("/exists/by", (req, res) => {
 		});
 });
 
+router.patch("/sessionUser/name", async (req, res) => {
+	const userId = req.session.passport?.user;
+	
+	if (!userId) {
+		return res.status(401).json();
+	}
+
+	const { firstname, lastname } = req.body
+	if (!firstname || !lastname) return res.status(400).json();
+
+	pgPool.query("UPDATE user_account SET firstname = $1, lastname = $2 WHERE id = $3 RETURNING id, email, firstName, lastName", [firstname, lastname, userId])
+	.then(users => {
+		const user = users.rows[0]
+		
+		return res.status(200).json({
+			id: user.id,
+			email: user.email,
+			firstName: user.firstname,
+			lastName: user.lastname
+		})
+	})
+	.catch(err => {
+		console.log(err.code)
+		return res.status(500).json();
+	})
+})
+
+router.patch("/sessionUser/email", async (req, res) => {
+	const userId = req.session.passport?.user;
+	
+	if (!userId) {
+		return res.status(401).json();
+	}
+
+	const { email } = req.body
+	if (!email) return res.status(400).json();
+
+	pgPool.query("UPDATE user_account SET email = $1 WHERE id = $2 RETURNING id, email, firstName, lastName", [email, userId])
+	.then(users => {
+		const user = users.rows[0]
+		
+		return res.status(200).json({
+			id: user.id,
+			email: user.email,
+			firstName: user.firstname,
+			lastName: user.lastname
+		})
+	})
+	.catch(err => {
+		console.log(err)
+		return res.status(500).json();
+	})
+})
+
+router.patch("/sessionUser/password", async (req, res) => {
+	const userId = req.session.passport?.user;
+	
+	if (!userId) {
+		return res.status(401).json();
+	}
+
+	const { password } = req.body
+	if (!password) return res.status(400).json();
+
+	let hashedPassword = await bcrypt.hash(password, 10)
+
+	pgPool.query("UPDATE user_account SET password = $1 WHERE id = $2 RETURNING id, email, firstName, lastName", [hashedPassword, userId])
+	.then(users => {
+		const user = users.rows[0]
+		
+		return res.status(200).json({
+			id: user.id,
+			email: user.email,
+			firstName: user.firstname,
+			lastName: user.lastname
+		})
+	})
+	.catch(err => {
+		console.log(err)
+		return res.status(500).json();
+	})
+})
+
+
+
 router.delete("/sessionUser", (req, res) => {
 	const userId = req.session.passport?.user;
 	
@@ -63,6 +149,7 @@ router.delete("/sessionUser", (req, res) => {
 		return res.status(401).json();
 	}
 
+	//TODO: delete watchlists associated with userId
 
 	pgPool.query("DELETE FROM user_account WHERE id = $1", [userId])
 	.then(() => {
